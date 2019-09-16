b7dccafafac077635d6716c31bda7e933a70c6db
youshy <devilyoushy@gmail.com>
2019-09-16 16:33:14 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// start the web server
+	log.Printf("Server is listening on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}

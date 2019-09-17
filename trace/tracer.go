7cc0ce38d13d0052695c13b87b3b055ef4dfe0de
youshy <devilyoushy@gmail.com>
2019-09-17 12:31:20 +0100

    TDD Tracer build

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,7 +1,25 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // Tracer is the interface that describes an object capable of
 // tracing events throughout code
 type Tracer interface {
 	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
 }
